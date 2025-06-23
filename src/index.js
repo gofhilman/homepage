8b26d27b5a35653501ac59ef4298540f15cccd4d
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-23 22:27:06 +0700

    feat: add typing style introduction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import introTyping from "./modules/intro-typing";
 import "./styles.css";
+
+const typing = document.querySelector("#typing");
+const cursor = document.querySelector("#cursor");
+
+setInterval(() => {
+  introTyping.type();
+  typing.textContent = introTyping.typedString;
+  cursor.textContent = introTyping.cursor;
+}, 100);
