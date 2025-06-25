c9af034f3932bd7f96e8fb58c22e45d3d95b7026
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-25 20:29:49 +0700

    WIP: create projects section html

--- src/index.js
+++ src/index.js
@@ -9,3 +9,51 @@ setInterval(() => {
   typing.textContent = introTyping.typedString;
   cursor.textContent = introTyping.cursor;
 }, 100);
+
+// Load YouTube IFrame API
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+document.body.appendChild(tag);
+
+const players = [];
+const videoData = [
+  { id: "battleship", videoId: "Lc8rXUNvyvE" },
+  { id: "weather-app", videoId: "gdUczWykRzM" },
+  { id: "todo-list", videoId: "vFe7MQMd6aE" },
+  { id: "meater", videoId: "Lsx4-RIvzX8" },
+  { id: "mstm", videoId: "gcjxURTSHHU" }
+];
+
+window.onYouTubeIframeAPIReady = () => {
+  console.log("YouTube API is ready!");
+  videoData.forEach((data, index) => {
+    // eslint-disable-next-line no-undef
+    players[index] = new YT.Player(data.id, {
+      height: "315",
+      width: "505",
+      videoId: data.videoId,
+      playerVars: {
+        autoplay: 0,
+        mute: 1,
+        loop: 1,
+        playlist: data.videoId,
+      },
+    });
+  });
+}
+
+// Scroll detection
+window.addEventListener("scroll", () => {
+  videoData.forEach((data, index) => {
+    const el = document.getElementById(data.id);
+    const rect = el.getBoundingClientRect();
+    const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+    const player = players[index];
+
+    if (isVisible && player?.playVideo) {
+      player.playVideo();
+    } else if (player?.pauseVideo) {
+      player.pauseVideo();
+    }
+  });
+});
